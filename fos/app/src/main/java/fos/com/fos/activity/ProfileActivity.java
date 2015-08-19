4be47c10a1144438e24374607282cb092f7aff5e
Swami <weelim@Swami-2.local>
2015-08-20 01:24:37 +0800

    Added profile refresh

--- fos/app/src/main/java/fos/com/fos/activity/ProfileActivity.java
+++ fos/app/src/main/java/fos/com/fos/activity/ProfileActivity.java
@@ -1,7 +1,10 @@
 package fos.com.fos.activity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ListView;
 import android.widget.Toast;
 import com.parse.FindCallback;
@@ -25,8 +28,16 @@ public class ProfileActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_profile);
     lvItem = (ListView) findViewById(R.id.lv_item);
+    profileAdapter = new ProfileAdapter(ProfileActivity.this, orderList);
+    lvItem.setAdapter(profileAdapter);
 
     setTitle(ParseUser.getCurrentUser().getUsername() + " - your order(s)");
+    loadOrders();
+  }
+
+  private void loadOrders() {
+    final ProgressDialog progressDialog = new ProgressDialog(this);
+    progressDialog.show();
     ParseQuery<ParseObject> parseQuery = new ParseQuery<ParseObject>("Order");
     parseQuery.whereEqualTo("userId", ParseUser.getCurrentUser());
     parseQuery.include("itemId");
@@ -34,14 +45,31 @@ public class ProfileActivity extends AppCompatActivity {
     parseQuery.findInBackground(new FindCallback<ParseObject>() {
       @Override public void done(List<ParseObject> list, ParseException e) {
         if (e == null) {
+          orderList.clear();
           orderList.addAll(list);
-          profileAdapter = new ProfileAdapter(ProfileActivity.this, orderList);
-          lvItem.setAdapter(profileAdapter);
+          profileAdapter.notifyDataSetChanged();
+          progressDialog.dismiss();
         } else {
           Toast.makeText(ProfileActivity.this, Constants.unexpected_error, Toast.LENGTH_LONG)
               .show();
         }
       }
     });
+  }
+
+  @Override public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.menu_profile, menu);
+    return true;
+  }
+
+  @Override public boolean onOptionsItemSelected(MenuItem item) {
+    int id = item.getItemId();
+    switch (id) {
+      case R.id.action_refresh:
+        loadOrders();
+        break;
+    }
+
+    return super.onOptionsItemSelected(item);
   }
 }
